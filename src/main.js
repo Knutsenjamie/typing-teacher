f5e8a067979653c00e8a2206b332b2c94da1f881
Vanessa Su <vnessa.su@gmail.com>
2021-04-26 19:22:11 -0400

    Add input text, file, and display
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- src/main.js
+++ src/main.js
@@ -2,4 +2,33 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Game from './js/game.js';
+// import Game from './js/game.js';
+
+$(document).ready(function(){
+  $('#inputTextbox').keypress(function(event){
+    let keypressEvent = String.fromCharCode(event.keyCode); 
+
+    let outputString;
+    if (keypressEvent.toUpperCase() === "A"){
+      outputString = `<textAr class="text-success">${keypressEvent}</span>`;
+      $('#inputTextbox').removeClass('is-invalid');
+      $('#inputTextbox').addClass('is-valid');
+    } else {
+      outputString = `<span class="text-danger">${keypressEvent}</span>`;
+      $('#inputTextbox').removeClass('is-valid');
+      $('#inputTextbox').addClass('is-invalid');
+    }
+    $('.showText').append(outputString);
+  });
+
+  $('#inputFile').change(function() {
+    console.log("A file was selected");
+    // const input = this;
+    const url = $(this).val();
+    console.log(url);
+  })
+});
+
+// jQuery.get(thefile), function(data) {
+//   $('#txtData').val(data);
+// }
