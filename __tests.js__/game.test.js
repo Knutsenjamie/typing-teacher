c2efab6bc9ff07ee8bd004ecc998004411a2d27d
James Wyn <thisisjameswyn@gmail.com>
2021-04-27 12:58:10 -0700

    change backend arguments
    Co-authored-by: Tori West <victoria_west@ymail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>

--- __tests.js__/game.test.js
+++ __tests.js__/game.test.js
@@ -4,25 +4,31 @@ describe('Game', () => {
   test('It should increment numberCorrect if the user enters the correct letter', () => {
     let game = new Game();
     game.setTextArray("console");
-    game.evalChar(99); //99=c
+    game.evalChar('c'); 
     expect(game.numberCorrect).toEqual(1);
   });
-  test('If an incorrect number is entered, set waitingForBackspace to true', () => {
+  test('If an incorrect character is entered, set waitingForBackspace to true', () => {
     let game = new Game();
     game.setTextArray("console");
-    game.evalChar(88); //88=x
+    game.evalChar('x'); 
     expect(game.waitingForBackspace).toEqual(true);
   });
   test('If waitingForBackspace, reset waitingForBackspace to false if backspace is entered', () => {
     let game = new Game();
     game.waitingForBackspace = true;
-    game.evalChar(8);
+    game.evalChar('Backspace');
     expect(game.waitingForBackspace).toEqual(false);
   });
-  test('If waitingForBackspace, reset waitingForBackspace to false if backspace is entered', () => {
+  test('If waitingForBackspace, waitingForBackspace stays equal to true if char other than backspace is entered', () => {
     let game = new Game();
     game.waitingForBackspace = true;
-    game.evalChar(67); //67=c
+    game.evalChar('c'); 
     expect(game.waitingForBackspace).toEqual(true);
   });
+  test('It should set textArray equal to input text')
+  // test('If numberCorrect and textArray length are equal then trigger gameOver = true', () => {
+  //   let game = new Game();
+  //   game.numberCorrect = ;
+    
+  // })
 });
--- src/js/game.js
+++ src/js/game.js
@@ -3,7 +3,7 @@ export default class Game {
     this.numberCorrect = 0;
     this.keystrokeCounter = 0;
     this.waitingForBackspace = false;
-    this.textArray = "";
+    this.textArray = [];
     this.gameOver = false;
   }
 
@@ -11,16 +11,15 @@ export default class Game {
     this.textArray=String(text).split('');
   }
 
-  evalChar(keyCode) {
+  evalChar(keystrokeChar) {
     this.keystrokeCounter++;
     if(this.waitingForBackspace === true) {
-      if(keyCode === 8)  { //8 === backspace
+      if(keystrokeChar === 'Backspace')  { //8 === backspace
         this.waitingForBackspace = false;
         } else {
         // recommend error message
       }
-    } else if (String.fromCharCode(keyCode) === this.textArray[this.numberCorrect]) {  //once we determine that's it's not a backspace, we should convert it into a character.  The character in the array will be ' ' for a space so if we convert it will we get ' ' === ' '
-      // so here we should have else if (String.fromCharCode(keyCode))
+    } else if (keystrokeChar === this.textArray[this.numberCorrect]) {  
       this.numberCorrect ++
       if(this.numberCorrect === this.textArray.length){
         this.gameOver = true;
@@ -33,23 +32,3 @@ export default class Game {
     return this.numberCorrect/this.keystrokeCounter;
   }
 }
-
-// keystrokeChar is a keycode like 8 for backspace
-// check for backspace for if waiting on backspace
-// if it's not backspace and we're not waiting on backspace we want to compare this key to the current letter of the textArray
-// keycode doesn't look like the character from textArray
-// keystrokeCodeConvertedToCharacter = String.fromCharCode(keystrokeChar); turns key code 65 into "a"
-
-// if32it's32not32backspace32and32we're32not32waiting32on32backspace we want to compare this key to the current letter of the textArray
-
-
-
-// THAT"S WHY WE NEED TO CONVERT IT AFTER WE DETERMINE IT"S NOT A BACKSPACE
-
-
-// let myChar = "\b" <--backspace char
-// undefined
-
-// myChar
-
-// "\u0008"  <--backspace utf code
